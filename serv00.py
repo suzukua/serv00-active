242ef3574d501939d8d42d786783b3d1fb80ff15
suzukua <464033315@qq.com>
2024-12-24 11:07:40 +0800

    脚本健壮性&青龙通知

--- serv00.py
+++ serv00.py
@@ -7,25 +7,31 @@ from urllib3.exceptions import InsecureRequestWarning
 warnings.simplefilter("ignore", InsecureRequestWarning)
 # 登录面板
 def LoginPanel():
-    res = requests.get(host, verify=False)
-    csrftoken = res.cookies['csrftoken']
     time.sleep(2)
     headers = {
         'Referer' : host,
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
         'Content-Type': 'application/x-www-form-urlencoded'
     }
-    data = {
-        'username': username,
-        'password': password,
-        'csrfmiddlewaretoken': csrftoken,
-    }
-    res = requests.post(host, data=data,headers=headers,cookies=res.cookies,verify=False)
-    if res.text.find('/logout/') != -1:
-        print('面板登录成功')
-    else:
-        print('面板登录失败')
-        os._exit(0)
+    msg = ''
+    try:
+        res = requests.get(host, verify=False)
+        csrftoken = res.cookies['csrftoken']
+        data = {
+            'username': username,
+            'password': password,
+            'csrfmiddlewaretoken': csrftoken,
+        }
+        res = requests.post(host, data=data,headers=headers,cookies=res.cookies,verify=False)
+        if res.text.find('/logout/') != -1:
+            msg = '面板登录成功'
+        else:
+            msg = '面板登录失败'
+    except Exception as e:
+        msg += f"面板登录异常: {e}"
+    print(msg)
+    return msg
+        
 # 登录ssh
 def LoginSsh():
     import paramiko
@@ -33,19 +39,25 @@ def LoginSsh():
     with paramiko.SSHClient() as ssh:
         # 自动添加SSH密钥
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        msg = ''
         print('开始ssh连接')
         try:
             # 连接服务器
             ssh.connect(hostname=hostname, username=username, password=password)
+            msg = 'ssh连接成功\n开始执行查看ip命令'
             print('ssh连接成功')
             print('开始执行查看ip命令')
             
             # 执行命令
             stdin, stdout, stderr = ssh.exec_command("curl ifconfig.me")
-            print(stdout.read().decode())
+            ip = stdout.read().decode()
+            print(ip)
+            msg += '\nIP:' + ip
         except Exception as e:
             print(f"SSH连接失败: {e}")
-            os._exit(0)
+            msg += f"SSH连接失败: {e}"
+        return msg
+        
 if __name__ == '__main__':
     # 从环境变量中获取服务器信息
     Serv00 = os.environ.get('Serv00')
@@ -58,5 +70,7 @@ if __name__ == '__main__':
     password = info[2]
     hostname_number = hostname.split('.')[0].replace('s', '')
     host = f'https://panel{hostname_number}.serv00.com/login/'
-    LoginPanel()
-    LoginSsh()
+    msg = LoginPanel()
+    msg += '\n' + LoginSsh()
+    QLAPI.notify('Serv00保活通知', msg)
+
